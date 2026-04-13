9e6d15a1656e0e7edeaf65caf6e77f9834772374
Adam Hsuan <adamhsuan5@gmail.com>
2026-04-12 21:35:09 -0700

    testing absolute uncertainty weighted avg

--- analysis.py
+++ analysis.py
@@ -407,8 +407,8 @@ def estimate_aggregated_masses_and_uncertainties(isotopes_info):
             predicted_parent_mass_unc = entry["predicted_parent_mass_unc"]
             if unc == 0 or predicted_parent_mass_unc == 0:
                 predicted_parent_mass_unc = 1e12  # arbitrarily large uncertainty to effectively ignore this value
-            sum += predicted_parent_mass / (predicted_parent_mass_unc / predicted_parent_mass) ** 2
-            denominator += 1 / (predicted_parent_mass_unc / predicted_parent_mass)** 2 
+            sum += predicted_parent_mass / (predicted_parent_mass_unc) ** 2
+            denominator += 1 / (predicted_parent_mass_unc)** 2 
             total_unc_denominator += 1 / (predicted_parent_mass)**2 
 
         # ensures no division by zero. completes average via error propegation formula
